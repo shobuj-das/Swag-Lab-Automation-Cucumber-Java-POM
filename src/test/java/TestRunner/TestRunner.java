a4ae0b659e425821f4d5880df34b3b2bd325d8f0
Shobuj Das <dasshobuj@gmail.com>
2025-04-17 00:43:18 +0600

    testsuite added

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -2,6 +2,7 @@ package TestRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/java/Features",
@@ -14,5 +15,13 @@ import io.cucumber.testng.CucumberOptions;
         }
 )
 
+//public class TestRunner extends AbstractTestNGCucumberTests {
+//}
+
 public class TestRunner extends AbstractTestNGCucumberTests {
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
